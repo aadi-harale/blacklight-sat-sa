d066410b98612c96101404d0dbfe4a38c273d86f
Aadi Harale <152193056+aadi-harale@users.noreply.github.com>
2026-09-07 04:04:09 +0530

    Surface the two-SOC falsification scenario in the workbench

--- app/workbench/page.tsx
+++ app/workbench/page.tsx
@@ -1,10 +1,12 @@
 import { ReviewOptimizer } from '@/components/ReviewOptimizer';
+import { SocChallengeBar } from '@/components/SocChallengeBar';
 import { Workbench } from '@/components/Workbench';
 
 export default function WorkbenchPage() {
   return (
     <>
       <a className="skip-link" href="#blacklight-workbench">Skip to assessment workspace</a>
+      <SocChallengeBar />
       <div id="blacklight-workbench" tabIndex={-1}>
         <Workbench />
       </div>
