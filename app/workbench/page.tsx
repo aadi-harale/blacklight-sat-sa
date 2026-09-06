49ad6a185f401b4adcb845b9af1e6fec6d9c13d3
Aadi Harale <152193056+aadi-harale@users.noreply.github.com>
2026-09-07 02:03:07 +0530

    Surface Review Optimizer in BLACKLIGHT workbench

--- app/workbench/page.tsx
+++ app/workbench/page.tsx
@@ -1,5 +1,11 @@
+import { ReviewOptimizer } from '@/components/ReviewOptimizer';
 import { Workbench } from '@/components/Workbench';
 
 export default function WorkbenchPage() {
-  return <Workbench />;
+  return (
+    <>
+      <Workbench />
+      <ReviewOptimizer />
+    </>
+  );
 }
